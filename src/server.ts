a7e96cd1b0cbce41c751cfa963528f1664070be0
PedroBittencourtCosta <119755420+PedroBittencourtCosta@users.noreply.github.com>
2023-10-08 13:12:42 -0300

    commit inicial

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import  Express  from 'express';
+
+const app = Express();
+
+
+app.use(Express.json());
+
+app.get('/', (req, res) => { res.json({ message: 'deu bom'}); });
+
+
+app.listen(3000, () => { console.log('reodando...');});
